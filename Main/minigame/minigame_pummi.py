737fff6d518cb36c58a69b48d3aa8ab4709fa087
robbie <linden.robbie98@gmail.com>
2025-03-06 11:30:32 +0200

    nyt tekee GBA äänet

--- Main/minigame/minigame_pummi.py
+++ Main/minigame/minigame_pummi.py
@@ -1,6 +1,16 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import gba_print
+import sound
+import random
+import pygame
+
 def minigame_pummi():
-    print("\nNäät lentokentässä pummin joka pyytää sinulta rahaa ruokaan")
-    print('Annatko pummille (x määrä) rahaa? Kyllä/En')
+    gba_print.slow_print("\nNäät lentokentässä pummin joka pyytää sinulta rahaa ruokaan")
+    gba_print.slow_print('Annatko pummille (x määrä) rahaa? Kyllä/En')
 
     while True:
         print("")
@@ -8,16 +18,18 @@ def minigame_pummi():
 
         if r in ["Kyllä", "En"]:
             break
-        print("Vastaa Kyllä/En")
+        gba_print.slow_print("Vastaa Kyllä/En")
 
     chance = random.randint(1, 10)
     win = chance in range(1, 3)
 
     if r == 'En':
-        print(
+        gba_print.slow_print(
             "\nJatkat matkaasi seuraavaan lentoon pikaisesti ja näät silmänkulmassasi kun pummi surullisesti tärisee nälästä")
     elif r == 'Kyllä':
         if win:
-            print("\nPummi oli sosiaalisen median vaikuttaja ja hän palkitsi sinut PISTEET sinun kiltteydestäsi.")
+            gba_print.slow_print("\nPummi oli sosiaalisen median vaikuttaja ja hän palkitsi sinut PISTEET sinun kiltteydestäsi.")
         else:
-            print("Pummi kiitti nauraen ja hyväksikäytti sinisilmäistä anteliaisuuttasi.")
+            gba_print.slow_print("Pummi kiitti nauraen ja hyväksikäytti sinisilmäistä anteliaisuuttasi.")
+
+minigame_pummi()
