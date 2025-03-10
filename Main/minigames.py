214880a857c02cc8ae10aedbccd08d6b2d466be2
Ville <ville.ahola@protonmail.com>
2025-03-10 12:52:52 +0200

    Korjattu mika carddraw toimiva versio minigames.py tiedostoon manchesterin kohdalle

--- Main/minigames.py
+++ Main/minigames.py
@@ -275,31 +275,48 @@ def helsinki():
 
 #Mika minipeli 2 "carddraw"
 def manchester():
-    while True:
-        print("Vieressä istuva aussi Hugh Jackman on tylistymässä kuoliaaksi ja "
-            "haastaa sinut pelaamaan kortin vetoa, isoin kortti voittakoon!")
-        player1 =random.randint(2,14)
-        player2 =random.randint(2,14)
-        if player1==player2:
-            coin=input(print("Ratkaistaan voittaja kolikolla, kruuna vai klaava?:  "))
-        if coin=="kruuna":
-            coin=1
-        if coin=="klaava":
-            coin=2
-        else:
-            print("Valitse kruuna tai klaava")
-        continue  # ohittaa break komennon jos käyttäjä ei vastaa oikein
-        random2=random.randint(1,2)
-        if coin==random2:
-            print("Arvasit oikein ja voitit!!")
-        else:
-            print("Voihan mielipaha arvasit väärin, hävisit tolle charmantille aussille!")
 
-        if player1>player2:
-            print(f"Hugh Jackmanin kortti: {player1} sinun korttisi {player2}")
-            print(f"Hugh Jackman voitti kortilla: {player1}. Äläkä ala mököttää, tapasit sentään charmantin julkkiksen ^_^  !")
-        else:
-            print(f"Pelaaja (tähän pelaajan nimimerkki) voitti Hugh Jackmanin.\nPalkinnoksi sait wolverinen muoviset raateluterät. Oiva lahja siskon pojalle! Pisteesi kerrotaan kertoimella 1.2!")
+    print("Vieressä istuva aussi Hugh Jackman on tylistymässä kuoliaaksi "
+        "ja haastaa sinut pelaamaan kortin vetoa, isoin kortti voittakoon!")
+    player_hugh = random.randint(2,14)
+    player_player = random.randint(2,14)
+    print("Charmantin aussin kortin arvo on ",player_hugh)
+    print("Sinun korttisi arvo on ", player_player)
+    if player_player < player_hugh:
+        print("Hävisit aussin korteille, et ansainnut pisteitä tästä tehtävästä! Mutta äläpä mutrua, \n"
+              "tapasit sentään charmantin julkkiksen <3")
+    if player_player > player_hugh:
+        print("Voitit korteillasi, sait palkinnoksi semipäheet Wolwerinen raateluhanskat. Oiva lahja siskon pojalle!\n"
+              "Pisteesi kerrotaan 1,2")
+        #kerro pisteet scoreboard
+    if player_player==player_hugh:
+        print("Teillä on samat kortit ja voittaja arvotaan kolikon heitolla")
+        question = query("Valitsetko kruunan vai klaavan?",
+                         ["1: Kruuna",
+                          "2: Klaava"],
+                         ["1", "2"])
+        if question == "1" :
+            coin = 0
+            coin += random.randint(1, 2)
+            if coin == 1 :
+                print("Tuli kruuna")
+                print("Arvasit oikein ja voitit. Sait palkinnoksi semipäheet Wolwerinen raateluhanskat.\nOiva lahja siskon pojalle!"
+              "Pisteesi kerrotaan 1,2"
+                      "")
+            else:
+                print(" Tuli klaava ja arvasit väärin. Hugh Jackman voitti. Älä Sure Tapasit sentään ihanan julkkiksen!")
+        if question =="2":
+            coin = 0
+            coin+=random.randint(1,2)
+            if coin == 2:
+                print("Tuli Klaava")
+                print("Arvasit oikein ja voitit tämänkin erän. Sait palkinnoksi semipäheet Wolwerinen raateluhanskat.\nOiva lahja siskon pojalle!"
+              "Pisteesi kerrotaan 1,3"
+                      "")
+
+            else:
+                print(
+                    "Tuli kruuna, arvasit väärin! Hugh Jackman voitti. Älä sure, tapasit sentään charmantin aussin <3")
 
 
 #Mika minipeli 3 "words"
