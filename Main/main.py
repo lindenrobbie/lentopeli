ff9a7aacd9ddf5821b6fc78aefa4139d05191c3a
Elias <elias.lee@metropolia.fi>
2025-03-11 15:01:50 +0200

    asdasdaf

--- Main/main.py
+++ Main/main.py
@@ -17,11 +17,13 @@ for i in result:
 airportgame={1:minigames.london,2:minigames.charles,3:minigames.amsterdam,4:minigames.frankfurt,5:minigames.rome,6:minigames.humberto,7:minigames.mallorca,8:minigames.athens,9:minigames.vienna,10:minigames.zurich,11:minigames.copenhagen,12:minigames.manchester,13:minigames.oslo,14:minigames.brussels,15:minigames.stockholm,16:minigames.warsaw,17:minigames.budapest,18:minigames.helsinki,19:minigames.tirana,20:minigames.keflavik,21:minigames.riga}
 #pelisilmukka, toistuu 10 kertaa
 for i in range(10):
+    maxid = db_modules.db_command("SELECT MAX(game_ID) FROM game")
     #haetaan 2 satunnaista lentokenttää, joissa pelaaja ei ole käynyt
     choice = db_modules.choose_airport() 
     airport1 = choice[0]
     airport2 = choice[1]
-
+    print(airport1)
+    print(airport2)
     #pelaaja valitsee lentokentän
     query = minigames.query("Mihin maahan haluaisit matkustaa?",
                     [f"1: {airport1[0][1]}", f"2: {airport2[0][1]}"],
@@ -32,8 +34,7 @@ for i in range(10):
     current_pos = airport1 if query == "1" else airport2
 
     #päivitetään valittu lentokenttä käydyksi sekä pelaajan sijainti valittuun lentokenttään
-    db_modules.travel_to(current_pos[0][0], 1) #parametreina siis lentokentän airport_id ja game_id, joka on aina 1 (jos resetoidaan db jokaisen pelin lopussa)
-    
+    db_modules.travel_to(current_pos[0][0], maxid[0][0]) 
 
     #tähän väliin vois lisätä komentoja esim. pisteiden tarkistamiseen
 
@@ -48,7 +49,6 @@ for i in range(10):
     minigame =airportgame[current_pos[0][0]]()
 
     #Luo maxid muuttujan ja antaa sen parametrinä modify_scorelle ja lisää/poistaa pisteet
-    maxid = db_modules.db_command("SELECT MAX(game_ID) FROM game")
     db_modules.modify_score(maxid[0][0],minigame[1])
 
     #Tulostaa uusimman tietokannan pelaajan nykyiset pisteet
