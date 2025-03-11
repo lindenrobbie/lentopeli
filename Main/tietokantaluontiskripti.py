4b2c391638707e2d455e14937808979dc3239151
Robbie Linden <robbie.linden@metropolia.fi>
2025-03-11 11:00:48 +0200

    korjasin mariadb connectoriin (sql syntax hajosi)

--- Main/tietokantaluontiskripti.py
+++ Main/tietokantaluontiskripti.py
@@ -19,55 +19,48 @@ def db_command(command):
 
 def install():
     return [
-        """CREATE DATABASE lentopeli;
-
-    USE lentopeli;
-
-    CREATE TABLE airport (
-        airport_ID INT NOT NULL PRIMARY KEY,
-        airport_name VARCHAR(255) NOT NULL,
-        airport_visited BOOLEAN DEFAULT FALSE
-    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-    CREATE TABLE game (
-        game_ID INT AUTO_INCREMENT PRIMARY KEY,
-        game_playername VARCHAR(100) NOT NULL,
-        game_playerscore INT NOT NULL,
-        game_playerpos INT NOT NULL,
-        FOREIGN KEY (game_playerpos) REFERENCES airport (airport_ID)
-    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-    CREATE TABLE scoreboard (
-        scoreboard_ID INT AUTO_INCREMENT PRIMARY KEY,
-        scoreboard_playername VARCHAR(100) NOT NULL,
-        scoreboard_finalscore INT NOT NULL
-    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-    INSERT INTO airport (airport_ID, airport_name, airport_visited)
-    VALUES
-    (1, 'London Heathrow Airport', FALSE),
-    (2, 'Charles de Gaulle International Airport', FALSE),
-    (3, 'Amsterdam Airport Schiphol', FALSE),
-    (4, 'Frankfurt am Main Airport', FALSE),
-    (5, 'Rome–Fiumicino Leonardo da Vinci International Airport', FALSE),
-    (6, 'Humberto Delgado Airport Lisbon Portela', FALSE),
-    (7, 'Palma de Mallorca Airport', FALSE),
-    (8, 'Athens Eleftherios Venizelos International Airport', FALSE),
-    (9, 'Vienna International Airport', FALSE),
-    (10, 'Zurich Airport', FALSE),
-    (11, 'Copenhagen Kastrup Airport', FALSE),
-    (12, 'Manchester Airport', FALSE),
-    (13, 'Oslo Airport, Gardermoen', FALSE),
-    (14, 'Brussels Airport', FALSE),
-    (15, 'Stockholm-Arlanda Airport', FALSE),
-    (16, 'Warsaw Chopin Airport', FALSE),
-    (17, 'Budapest Liszt Ferenc International Airport', FALSE),
-    (18, 'Helsinki Vantaa Airport', FALSE),
-    (19, 'Tirana International Airport Mother Teresa', FALSE),
-    (20, 'Keflavik International Airport', FALSE),
-    (21, 'Riga International Airport', FALSE);
-
-    ALTER DATABASE lentopeli CHARACTER SET utf8 COLLATE utf8_unicode_ci;"""
+        "CREATE DATABASE IF NOT EXISTS lentopeli;",
+        "USE lentopeli;",
+        """CREATE TABLE IF NOT EXISTS airport (
+            airport_ID INT NOT NULL PRIMARY KEY,
+            airport_name VARCHAR(255) NOT NULL,
+            airport_visited BOOLEAN DEFAULT FALSE
+        ) ENGINE=InnoDB DEFAULT CHARSET=latin1;""",
+        """CREATE TABLE IF NOT EXISTS game (
+            game_ID INT AUTO_INCREMENT PRIMARY KEY,
+            game_playername VARCHAR(100) NOT NULL,
+            game_playerscore INT NOT NULL,
+            game_playerpos INT NOT NULL,
+            FOREIGN KEY (game_playerpos) REFERENCES airport (airport_ID)
+        ) ENGINE=InnoDB DEFAULT CHARSET=latin1;""",
+        """CREATE TABLE IF NOT EXISTS scoreboard (
+            scoreboard_ID INT AUTO_INCREMENT PRIMARY KEY,
+            scoreboard_playername VARCHAR(100) NOT NULL,
+            scoreboard_finalscore INT NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=latin1;""",
+        """INSERT INTO airport (airport_ID, airport_name, airport_visited) VALUES
+        (1, 'London Heathrow Airport', FALSE),
+        (2, 'Charles de Gaulle International Airport', FALSE),
+        (3, 'Amsterdam Airport Schiphol', FALSE),
+        (4, 'Frankfurt am Main Airport', FALSE),
+        (5, 'Rome–Fiumicino Leonardo da Vinci International Airport', FALSE),
+        (6, 'Humberto Delgado Airport Lisbon Portela', FALSE),
+        (7, 'Palma de Mallorca Airport', FALSE),
+        (8, 'Athens Eleftherios Venizelos International Airport', FALSE),
+        (9, 'Vienna International Airport', FALSE),
+        (10, 'Zurich Airport', FALSE),
+        (11, 'Copenhagen Kastrup Airport', FALSE),
+        (12, 'Manchester Airport', FALSE),
+        (13, 'Oslo Airport, Gardermoen', FALSE),
+        (14, 'Brussels Airport', FALSE),
+        (15, 'Stockholm-Arlanda Airport', FALSE),
+        (16, 'Warsaw Chopin Airport', FALSE),
+        (17, 'Budapest Liszt Ferenc International Airport', FALSE),
+        (18, 'Helsinki Vantaa Airport', FALSE),
+        (19, 'Tirana International Airport Mother Teresa', FALSE),
+        (20, 'Keflavik International Airport', FALSE),
+        (21, 'Riga International Airport', FALSE);""",
+        "ALTER DATABASE lentopeli CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
     ]
 
 #Toistaa kunnes syöttää tyhjän kentän, A = Asentaa, P = Poistaa
