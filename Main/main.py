c8d92a2479fd3d49eceef193060c8cefd9d5ee94
Elias <elias.lee@metropolia.fi>
2025-03-11 14:09:39 +0200

    aasdasad

--- Main/main.py
+++ Main/main.py
@@ -1,6 +1,6 @@
 #Tässä on main class tiedosto lentopelille
 
-import ascii_art, db_modules, scoreboard, minigames
+import db_modules, scoreboard, minigames
 
 db_modules.db_command("update airport set airport_visited = False")
 
@@ -16,7 +16,7 @@ for i in result:
 #Minipelien sanakirja
 airportgame={1:minigames.london,2:minigames.charles,3:minigames.amsterdam,4:minigames.frankfurt,5:minigames.rome,6:minigames.humberto,7:minigames.mallorca,8:minigames.athens,9:minigames.vienna,10:minigames.zurich,11:minigames.copenhagen,12:minigames.manchester,13:minigames.oslo,14:minigames.brussels,15:minigames.stockholm,16:minigames.warsaw,17:minigames.budapest,18:minigames.helsinki,19:minigames.tirana,20:minigames.keflavik,21:minigames.riga}
 #pelisilmukka, toistuu 10 kertaa
-for i in range(10):
+for i in range(2):
     #haetaan 2 satunnaista lentokenttää, joissa pelaaja ei ole käynyt
     choice = db_modules.choose_airport() 
     airport1 = choice[0]
