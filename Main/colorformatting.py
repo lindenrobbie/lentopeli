7e4de16642db805b6a38881bb075c3d52dad2fc7
robbie <linden.robbie98@gmail.com>
2025-03-12 09:51:19 +0200

    ansi värit jos joku haluaa käyttää

---
+++ Main/colorformatting.py
@@ -0,0 +1,14 @@
+#tästä saa värit tekstiin jos haluaa:
+
+"""Black	    \033[30m
+Red	        \033[31m
+Green	    \033[32m
+Yellow	    \033[33m
+Blue	    \033[34m
+Magenta	    \033[35m
+Cyan	    \033[36m
+White	    \033[37m
+Default	    \033[39m"""
+
+print("\033[31mTässä on esimerkki punasesta\033[39m ja tässä se vaihtuu takaisin defaulttiin")
+
